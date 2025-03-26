244bac330d5405184585f747343776679ae66418
VANAFA <127407846+VANAFA@users.noreply.github.com>
2025-03-26 12:48:50 -0300

    Update sim.c

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -1,81 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-#include "shell.h"
-
-typedef enum {
-    LSL,
-    LSR,
-    ASR
-} ShiftType;
-
-ShiftType DecodeShift(uint32_t shift) {
-    switch (shift) {
-        case 0:
-            return LSL;
-        case 1:
-            return LSR;
-        case 2:
-            return ASR;
-        default:
-            // Reserved value, HALT
-            RUN_BIT = 0;
-            return LSL; // Default return to avoid compiler warning
-    }
-}
-
-void adc(int64_t* xd, int64_t* xn, int64_t* xm) {
-    // CURRENT_STATE.REGS[*xd] = CURRENT_STATE.REGS[*xn] + CURRENT_STATE.REGS[*xm];
-    NEXT_STATE.REGS[*xd] = CURRENT_STATE.REGS[*xn] + *xm;
-    // mem_write_32(NEXT_STATE.REGS[*xd], CURRENT_STATE.REGS[*xn] + *xm);
-    // NEXT_STATE.FLAG_N = CURRENT_STATE.FLAG_N;
-    // NEXT_STATE.FLAG_Z = CURRENT_STATE.FLAG_Z;
-    
-    int64_t result = CURRENT_STATE.REGS[*xn] + *xm;
-    NEXT_STATE.REGS[*xd] = result;
-
-    // Update flags
-    // Negative flag (N): Set if the result is negative
-    if (result < 0) {
-        NEXT_STATE.FLAG_N = 1;
-    } else {
-        NEXT_STATE.FLAG_N = 0;
-    }
-
-    // Zero flag (Z): Set if the result is zero
-    if (result == 0) {
-        NEXT_STATE.FLAG_Z = 1;
-    } else {
-        NEXT_STATE.FLAG_Z = 0;
-    }
-}
-
-void adds_shifted_register(int64_t* xd, int64_t* xn, int64_t* xm) {
-    // CURRENT_STATE.REGS[*xd] = CURRENT_STATE.REGS[*xn] + CURRENT_STATE.REGS[*xm];
-    NEXT_STATE.REGS[*xd] = CURRENT_STATE.REGS[*xn] + *xm;
-    // mem_write_32(NEXT_STATE.REGS[*xd], CURRENT_STATE.REGS[*xn] + *xm);
-    // NEXT_STATE.FLAG_N = CURRENT_STATE.FLAG_N;
-    // NEXT_STATE.FLAG_Z = CURRENT_STATE.FLAG_Z;
-    
-    int64_t result = CURRENT_STATE.REGS[*xn] + *xm;
-    NEXT_STATE.REGS[*xd] = result;
-
-    // Update flags
-    // Negative flag (N): Set if the result is negative
-    if (result < 0) {
-        NEXT_STATE.FLAG_N = 1;
-    } else {
-        NEXT_STATE.FLAG_N = 0;
-    }
-
-    // Zero flag (Z): Set if the result is zero
-    if (result == 0) {
-        NEXT_STATE.FLAG_Z = 1;
-    } else {
-        NEXT_STATE.FLAG_Z = 0;
-    }
-}
-
+#include "fun.c"
 
 void process_instruction () {
     /* execute one instruction here. You should use CURRENT_STATE and modify
@@ -84,97 +10,122 @@ void process_instruction () {
      * */
 
     uint32_t instruction = mem_read_32(CURRENT_STATE.PC);
-    // uint32_t opcode = (instruction >> 21) & 0x7FF;
-    // printf("Opcode: 0x%X\n", opcode);
-    // printf("Instruction: 0x%X\n", instruction);
-    // for (int i = 31; i >= 0; i--) {
-    //     printf("%d", (instruction >> i) & 1);
-    // }
-    // printf("\n");
 
-    // uint32_t part1 = instruction >> 24;
-    // uint32_t part2 = (instruction >> 21) & 0b1;
-    // printf("Part1: ");
-    // for (int i = 7; i >= 0; i--) {
-    //     printf("%d", (part1 >> i) & 1);
-    // }
-    // printf(", Part2: ");
-    // for (int i = 0; i >= 0; i--) {
-    //     printf("%d", (part2 >> i) & 1);
-    // }
-    // printf("\n");
+    // printf("%x\n", instruction >> 24);
+    // printf("%x\n", (instruction >> 21) & 1);
+    // printf("%x\n", (instruction));
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (instruction >> i) & 1);
+        if (i % 4 == 0) printf(" ");
+    }
+    printf("\n");
 
-    printf("%x\n", instruction >> 24);
-    printf("%x\n", (instruction >> 21) & 1);
     if (instruction >> 24 == 0b10110001) { // adds (immediate) C6-531
         printf("I'm in adds immediate\n");
+        adds_immediate(instruction);
 
-        int64_t rd = instruction & 0x1F;
-        int64_t rn = (instruction >> 5) & 0x1F;
-        int64_t shift = (instruction >> 22) & 0x3;
-        int64_t imm12 = (instruction >> 10) & 0xFFF;
+    } else if (instruction >> 21 == 0b10101011001) { // adds (extended register) C6-529
+        printf("I'm in adds extended register\n");
+        adds_extended_register(instruction);
 
-        switch (shift) {
-            case 0:
-                break;
-            case 1:
-                imm12 <<= 12;
-                break;
-            default:
-                // Reserved value, HALT
-                RUN_BIT = 0;
-                return;
+    } else if (instruction >> 21 == 0b11101011001) {
+        if ((instruction & 0x1F) == 0b11111) { // cmp (extended register) C6-589
+            printf("I'm in cmp extended register\n");   
+            cmp_extended_register(instruction);
+
+        } else {                                 // subs (extended register) C6-934
+            printf("I'm in subs extended register\n");
+            subs_extended_register(instruction);
+
         }
-
-        adds_shifted_register(&rd, &rn, &imm12);
-
-        // printf("rd: %ld, rn: %ld, imm12: %ld\n", rd, rn, imm12);
+    } else if (instruction >> 24 == 0b11110001) {
+        if ((instruction & 0x1F) == 0b11111) { // cmp (immediate) C6-591
+            printf("I'm in cmp immediate\n");
+            cmp_immediate(instruction);
+        } else {                              // subs (immediate) C6-936
+            printf("I'm in subs immediate\n");
+            subs_immediate(instruction);
+        }
     } else if (instruction >> 24 == 0b10101011 && ((instruction >> 21) & 1) == 0) { // adds (shifted register) C6-533
         printf("I'm in adds shifted register\n");
+        adds_shifted_register(instruction);
 
-        int64_t rd = instruction & 0x1F;
-        int64_t rn = (instruction >> 5) & 0x1F;
-        int64_t rm = (instruction >> 16) & 0x1F;
-        int64_t shift = (instruction >> 22) & 0x3;
-        int64_t imm6 = (instruction >> 10) & 0x3F;
-
-        if (shift == 0b11) {
-            // Reserved value, HALT
-            RUN_BIT = 0;
-            return;
-        }
-
-        ShiftType shift_type = DecodeShift(shift);
-        int64_t shift_amount = imm6;
-
-        int64_t shifted_rm = CURRENT_STATE.REGS[rm];
-        switch (shift_type) {
-            case LSL:
-                shifted_rm <<= shift_amount;
-                break;
-            case LSR:
-                shifted_rm = (uint64_t)shifted_rm >> shift_amount;
-                break;
-            case ASR:
-                shifted_rm >>= shift_amount;
-                break;
-            default:
-                // Reserved value, HALT
-                RUN_BIT = 0;
-                return;
-        }
-
-        adds_shifted_register(&rd, &rn, &shifted_rm);
-
-
-    // } else if () { // subs (shifted register) C6-938
+    } if (instruction >> 21 == 0b11101010000) { // ands (shifted register) C6-542
+        printf("I'm in ands\n");
+        ands(instruction);
         
+    // } else if (instruction >> 21 == 0b11101011001 && (instruction & 0x1F) == 0x1F) { // cmp (extended register) C6-589
+    //     printf("I'm in cmp extended register\n");
+
+    //     int64_t n = (instruction >> 5) & 0x1F;
+    //     int64_t m = (instruction >> 16) & 0x1F;
+
+    //     cmp_extended_register(&n, &m);
+    // } else if (instruction >> 24 == 0b11110001) { // cmp (immediate) C6-591
+    //     if ((instruction & 0x1F) == 0b11111) {
+    //         printf("I'm in cmp extended register\n");
+
+    //         int64_t n = (instruction >> 5) & 0x1F;
+    //         int64_t m = (instruction >> 16) & 0x1F;
+
+    //         cmp_extended_register(&n, &m);
+    //     } else {
+    //         printf("I'm in cmp immediate\n");
+
+    //         int64_t n = (instruction >> 5) & 0x1F;
+    //         int64_t imm12 = (instruction >> 10) & 0xFFF;
+
+    //         cmp_immediate(&n, &imm12);
+    //     }
+    } else if (instruction >> 22 == 0b1101001000) { // eor (immediate)
+        printf("I'm in eor immediate\n");
+
+        int64_t d = instruction & 0x1F;
+        int64_t n = (instruction >> 5) & 0x1F;
+        int64_t imm12 = (instruction >> 10) & 0xFFF;
+
+        // Perform the EOR operation with the immediate value
+        int64_t result = CURRENT_STATE.REGS[n] ^ imm12;
+
+        // Store the result in the destination register
+        NEXT_STATE.REGS[d] = result;
+
+        // Update flags
+        check_flags(result);
+    } else if (instruction >> 21 == 0b11101010010) { // orr (shifted register)
+        printf("I'm in orr\n");
+
+        int64_t d = instruction & 0x1F;
+        int64_t n = (instruction >> 5) & 0x1F;
+        int64_t m = (instruction >> 16) & 0x1F;
+
+        orr_shift_reg(&d, &n, &m);
+    } else if (instruction >> 26 == 0b000101) { // b C6-550
+        printf("I'm in b\n");
+
+        int32_t imm26 = instruction & 0x3FFFFFF;
+
+        b_target(imm26);
+    } else if (instruction >> 10 == 0b1101011000011111000000 && (instruction & 0xF) == 0b00000) { // br C6-562
+        printf("I'm in br\n");
+
+        int64_t xn = instruction & 0x1F;
+
+        br(&xn);
+    } else if (instruction >> 24 == 0b01010100 && ((instruction >> 4) & 1) == 0) { // b.cond C6-549
+        printf("I'm in b conditional\n");
+
+        int32_t imm19 = instruction & 0x7FFFF;
+        int cond = (instruction >> 12) & 0xF;
+
+        b_conditional(imm19, cond);
     }
 
     CURRENT_STATE.PC += 4;
     NEXT_STATE.PC = CURRENT_STATE.PC;
 
     if (instruction == 0xd4400000) { // HLT 0
+        printf("HLT\n");
         RUN_BIT = 0;
     }
 }
